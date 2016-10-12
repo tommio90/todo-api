1288a2354d5fcf0d9b0495294f73526cfd59c090
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-10-12 12:19:39 +0800

    todos added

--- server.js
+++ server.js
@@ -2,6 +2,44 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+    id: 1,
+    description: 'Meet mum for lunch',
+    completed: false
+
+},{
+    id: 2,
+    description: 'go to market',
+    completed: false
+},{
+    id: 3,
+    description: 'study node.js',
+    completed: true
+}];
+
+//GET /todos
+app.get('/todos', function(req, res){
+    res.json(todos);
+})
+//GET /todos/:id
+app.get('/todos/:id', function(req,res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+    
+    todos.forEach(function(todo) {
+        if(todoId === todo.id){
+           machedTodo = todo;
+        }
+        return matchedTodo;
+    });
+      
+    if(matchedTodo){
+       res.json(matchedTodo); 
+    }else{
+        res.status(484).send();
+    };
+
+})
 
 app.get('/', function (req, res){
 
