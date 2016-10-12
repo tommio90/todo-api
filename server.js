1caeb1cd0fd122f12ebf5423fc3837c8bc939e2b
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-10-12 19:07:59 +0800

    Add delete root

--- server.js
+++ server.js
@@ -46,7 +46,22 @@ app.post('/todos', function(req, res){
     res.json(body);
 });
 
+// DELETE /todos/:id
 
+app.delete('/todos/:id', function(req,res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    if(!matchedTodo){
+       
+        res.status(404).json({"Error" : "Not todos found for that Id"}); 
+     
+    }else{
+       
+       todos=  _.without(todos, matchedTodo); 
+      res.json(matchedTodo);
+    }
+
+});
 
 
 app.listen(PORT, function(){
