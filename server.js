0cf34778826eadea288eac10b613e70d2451968b
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-10-13 16:27:48 +0800

    added complete filter

--- server.js
+++ server.js
@@ -9,10 +9,19 @@ var todoNextId =1;
 
 app.use(bodyParser.json());
 
-//GET /todos
+//GET /todos?completed=true
 app.get('/todos', function(req, res){
-    res.json(todos);
-})
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+   	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+    res.json(filteredTodos);
+});
 //GET /todos/:id
 app.get('/todos/:id', function(req,res){
     var todoId = parseInt(req.params.id, 10);
