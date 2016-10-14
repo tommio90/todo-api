eeaca7b53dd551458884764a3cd71c789e1d9fbd
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-10-14 14:24:53 +0800

    conver get todos id to use sequelize

--- server.js
+++ server.js
@@ -31,20 +31,20 @@ app.get('/todos', function(req, res){
 //GET /todos/:id
 app.get('/todos/:id', function(req,res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
-    if(matchedTodo){
-       res.json(matchedTodo); 
-    }else{
-        res.status(484).send();
-    };
+     db.todo.findById(todoId).then(function(todo){
+         if(!!todo){
+            res.json(todo.toJSON());
+         }else{
+            res.status(404).send();
+         }
+     }), function(e){
+         res.status(500).send();
+     }
+   
 
 });
 
-app.get('/', function (req, res){
 
-    res.send('Todo API root');
-
-});
 //POST /todos
 
 app.post('/todos', function(req, res){
@@ -56,22 +56,7 @@ app.post('/todos', function(req, res){
         res.status(400).json(e);
     });
 });
-                    
-
- 
-    /*
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
-    body.description = body.description.trim();
-    body.id = todoNextId ++;
-
-    todos.push(body);
-    
-    res.json(body);
-});
-*/
-// DELETE /todos/:id
+            
 
 app.delete('/todos/:id', function(req,res){
     var todoId = parseInt(req.params.id, 10);
