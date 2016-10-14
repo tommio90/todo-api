5aceb82d560ad8af08930242b92408813d64c3fc
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-10-14 13:47:51 +0800

    added the db.js todo.js and updated put in the server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var bodyParser = require('body-parser')
 var _ = require('underscore');
+var db = require('./db.js');
 
 var todos = [];
 var todoNextId =1;
@@ -48,7 +49,17 @@ app.get('/', function (req, res){
 
 app.post('/todos', function(req, res){
     var body = _.pick(req.body, 'description', 'completed');
-    
+        
+    db.todo.create(body).then(function(todo){
+        res.json(todo.toJSON());
+    }, function(e) {
+        res.status(400).json(e);
+    });
+});
+                    
+
+ 
+    /*
     if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
@@ -59,7 +70,7 @@ app.post('/todos', function(req, res){
     
     res.json(body);
 });
-
+*/
 // DELETE /todos/:id
 
 app.delete('/todos/:id', function(req,res){
@@ -105,8 +116,10 @@ app.put('/todos/:id', function (req, res) {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function () {
-	console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
 
 
