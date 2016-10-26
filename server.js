993679616545966aee09ee076a9ed85af446a17d
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-10-26 17:16:41 +0800

    added token

--- server.js
+++ server.js
@@ -151,7 +151,12 @@ app.post('/users/login', function(req, res){
     var body = _.pick(req.body, 'email', 'password');
 
     db.user.authenticate(body).then(function(user){
-        res.json(user.toPublicJSON());
+        var token = user.generateToken('authentication');
+        if(token){
+            res.header('Auth', token ).json(user.toPublicJSON());
+        }else{
+            res.status(401).send();
+        }
     }, function(){
         res.status(401).send();
     });
@@ -164,7 +169,7 @@ app.post('/users/login', function(req, res){
 
 
 
-db.sequelize.sync(force = true).then(function() {
+db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
