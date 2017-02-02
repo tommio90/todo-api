8734c254ed7f0bfe404bde34cbbd4bdb5b65c848
Giuseppe Tomasello <gtomasello90@gmail.com>
2017-02-02 22:29:06 +0800

    managed to connect to the ORM

--- db.js
+++ db.js
@@ -9,9 +9,10 @@ var port  =process.env.RDS_PORT;
 var database = process.env.RSD_DB_NAME;
 
 // if(env === 'production'){
-     sequelize = new Sequelize('Edugora', 'root', 'messenger', {
+    
+    var sequelize = new Sequelize('play', 'root', 'password', {
         host: "localhost",
-        port: 3306
+        dialect: 'mysql'
 });
 // }else{
 
@@ -22,15 +23,25 @@ var database = process.env.RSD_DB_NAME;
  
 // }
 var db = {};
-db.word_test = sequelize.import(__dirname + '/models/word_test.js');
-db.worddb = sequelize.import(__dirname + '/models/worddb.js');
-db.wordbook = sequelize.import(__dirname + '/models/wordbook.js');
-db.wordzh = sequelize.import(__dirname + '/models/wordzh.js');
+db.uok = sequelize.import(__dirname + '/models/uok.js');
+db.units = sequelize.import(__dirname + '/models/units.js');
+db.uok_relationship = sequelize.import(__dirname + '/models/uok_relationship.js');
+db.cedict = sequelize.import(__dirname + '/models/cedict.js');
+db.hsk = sequelize.import(__dirname + '/models/hsk_seq.js');
+db.sentence = sequelize.import(__dirname + '/models/sentence.js');
+
+db.mywords = sequelize.import(__dirname + '/models/mywords.js');
+db.many_to_many = sequelize.import(__dirname + '/models/many_to_many.js');
+
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.user = sequelize.import(__dirname + '/models/user.js');
 db.token = sequelize.import(__dirname + '/models/token.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+
+db.uok_relationship.belongsToMany(db.user, {through: db.many_to_many});
+db.user.belongsToMany(db.uok_relationship, {through: db.many_to_many});
 
 db.todo.belongsTo(db.user);
 db.user.hasMany(db.todo);
--- services/translate.js
+++ services/translate.js
@@ -1,14 +1,12 @@
 var request = require('request');
-var pinyin = require("chinese-to-pinyin");
+
 var hasChinese = require('has-chinese');
 module.exports = function (translation) {
 	return new Promise(function (resolve, reject) {
 		var encodedTranslation = encodeURIComponent(translation);
 		var url = 'http://fanyi.youdao.com/openapi.do?keyfrom=edugora&key=1877262899&type=data&doctype=json&version=1.1&q='+ encodedTranslation;
 
-		if (!translation) {
-			return reject('No translation provided');
-		}
+	
 
 		request({
 			url: url,
@@ -18,18 +16,14 @@ module.exports = function (translation) {
 				reject('Unable to translate');
 			} else {
         var hanziString = JSON.stringify(body.translation);
-         if(hasChinese(hanziString)){
-            var sound = pinyin(hanziString);
-            resolve('Translation is: ' + body.translation + '  Pinyin: '+ sound );
-        
-         }else{
-          
+  
            var hanziString = JSON.stringify(body.query);
-           var sound = pinyin(hanziString);
+           //var sound = pinyin(hanziString);
 		   var traduzione = JSON.stringify(body.translation);
-		   var word ={hanzi: body.query, pinyin_tone: sound, translation: traduzione };
+		   var explain = JSON.stringify(body);
+		   var word ={hanzi: body.query, translation: traduzione };
            resolve(word);
-         }
+         
 			}
 		});
 		
